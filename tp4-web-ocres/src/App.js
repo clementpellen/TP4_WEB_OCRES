3eb32db114a44ce2ca7333855b84c0c6c846f72f
clementpellen <clementpellen@gmail.com>
2021-10-17 16:15:21 +0200

    header + main done

--- tp4-web-ocres/src/App.js
+++ tp4-web-ocres/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import profile from "../src/profile.png";
 import './App.css';
+
+const profils = [
+{
+  nom: ["Jeanne", "Dupont"],
+  birthday: [22, 2, 1983],
+  profile_image: profile,
+},
+{
+  nom: ["Martine", "Cabot"],
+  birthday: [13, 11, 1989],
+  profile_image: profile,
+},
+{
+  nom: ["Claude", "Lellouche"],
+  birthday: [17, 1, 1969],
+  profile_image: profile,
+}
+]
+
+var profile_id = 0;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
     </div>
   );
+}
+
+function Main() {
+  return (
+    <main>
+      <img src={profils[profile_id].profile_image} alt="profile"></img>
+        <div className='names'>
+          <h2>{profils[profile_id].nom[0]}</h2>
+          <h2>{profils[profile_id].nom[1]}</h2>
+        </div>
+        <h2>{profils[profile_id].birthday[0]}/{profils[profile_id].birthday[1]}/{profils[profile_id].birthday[2]}</h2>
+    </main>
+  )
+}
+
+function Header() {
+  console.log("ICII");
+  return(
+    <header>
+      <button >{profils[0].nom[0]}</button>
+      <button >{profils[1].nom[0]}</button>
+      <button >{profils[2].nom[0]}</button>
+    </header>
+  )
+}
+
+// onClick={() => selectProfile(0)}
+// onClick={() => selectProfile(1)}
+// onClick={() => selectProfile(2)}
+
+function selectProfile(id) {
+  profile_id = id;
 }
 
 export default App;
